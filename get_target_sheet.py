d74deaf5d6fe5ab587f8dc6b7a87b13e68e3af3e
lvmt <13554221497@163.com>
2022-01-16 22:11:50 +0800

    获取excel中指定sheet

---
+++ get_target_sheet.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   get_target_sheet.py
+@Time    :   2021/10/18 16:32:59
+@Author  :   lvmt 
+@Version :   1.0
+'''
+
+""" 
+从xlsx文件中，读取指定sheet
+"""
+
+
+# here put the import lib
